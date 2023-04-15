c6825015c4241352b0b4dfb752767bb49dd731fb
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-04-14 17:20:42 -0700

    personalization done

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Built with Starter React Portfolio Template via Dorota1997/react-frontend-dev-portfolio
 
+## Color Theme Provided by Adobe.com, Samuel Alexander AW19 Collection
+
 ### URL: <https://voluble-marigold-2fd67d.netlify.app/>
 
 >Number and name of feature: 1. Clone Template & Deploy
@@ -26,30 +28,30 @@
 
 >Number and name of feature: 3. Projects Section
 >
->Estimate of time needed to complete: 
+>Estimate of time needed to complete: 1 hour
 >
->Start time: 
+>Start time: 3:30pm
 >
->Finish time: 
+>Finish time: 4pm
 >
->Actual time needed to complete: 
+>Actual time needed to complete: 30 minutes
 
 >Number and name of feature: 4. Skills and Experience Sections
 >
->Estimate of time needed to complete: 
+>Estimate of time needed to complete: 1 hour
 >
->Start time: 
+>Start time: 4pm
 >
->Finish time: 
+>Finish time: 4:15pm
 >
->Actual time needed to complete: 
+>Actual time needed to complete: 15 minutes
 
 >Number and name of feature: 5. Personalize
 >
->Estimate of time needed to complete: 
+>Estimate of time needed to complete: 1 hour
 >
->Start time: 
+>Start time: 4:15pm
 >
->Finish time: 
+>Finish time: 5:15pm
 >
->Actual time needed to complete: 
+>Actual time needed to complete: 1 hour
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import javascriptIcon from "@iconify/icons-logos/javascript";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import html5Icon from "@iconify/icons-logos/html-5";
 
 class About extends Component {
   render() {
@@ -26,20 +26,20 @@ class About extends Component {
               <div className="polaroid">
                 <span style={{ cursor: "auto" }}>
                   <img
-                    height="250px"
+                    height="auto"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
+                    <Icon
+                      icon={html5Icon}
+                      style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    />
                   <Icon
-                    icon={angularIcon}
+                    icon={javascriptIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={vueIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
